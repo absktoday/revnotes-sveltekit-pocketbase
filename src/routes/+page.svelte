77611ae6437714e57987ec04029525bb6ea9f3b7
Abhishek Saral <abhisheksaral17@gmail.com>
2024-05-02 22:40:51 -0700

    fix typo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <div class="container h-screen mx-auto flex flex-col justify-center items-center space-y-5 px-3">
 	<h1 class="h1">The simplest way to keep notes</h1>
 	<p>All your notes,synced to all your devices. Use RevNotes now in your browser.</p>
-	<a href="/secure/dashbaord" class="btn variant-filled">Open in Browser</a>
+	<a href="/secure/dashboard" class="btn variant-filled">Open in Browser</a>
 </div>
